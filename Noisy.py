12706af89b24105eaa5e6e20ace9438c9a44962c
noarche <n0arch@pm.me>
2024-08-06 17:32:44 -0400

    Update Noisy.py

--- Noisy.py
+++ Noisy.py
@@ -15,7 +15,6 @@ UA = UserAgent(min_percentage=15.1)
 REQUEST_COUNTER = -1
 SYS_RANDOM = random.SystemRandom()
 
-
 total_bandwidth = 0
 
 main_logo = '''
@@ -35,10 +34,20 @@ main_logo = '''
       \033[94m    Traffic Obfuscation     \033[0m
       \033[95m****************************\033[0m
 
-\033[94m Visit the github for more information. \033[0m
+\033[96m Docker Build Date: August 6 2024 \033[0m
+
+\033[34m This docker image has 4 configs, use no arguements for the default config \033[0m
+\033[31m Available arguments for other configs: \033[0m
+\033[32m --config configHighBandwidth.json \033[0m
+\033[32m --config configLowBandwidth.json \033[0m
+\033[32m --config configLowBandwidthFast.json \033[0m
+
+\033[34m Visit the github for more information. \033[0m
+
 
 '''
 print(main_logo)
+
 class Crawler:
     def __init__(self):
         self._config = {}
@@ -88,11 +97,21 @@ class Crawler:
         )
         return re.match(regex, url) is not None
 
+    @staticmethod
+    def _contains_unwanted_strings(url):
+        unwanted_strings = ['.ico', '.png', '.jpg', '.webp', '.webm', '.pdf', '.doc', '.docx', '.svg', '.jpeg', '.json', '.onion', '.i2p', '.safetensors', '.rar', '.zip', '.gguf', '.ggml', '.shp', '.gif', '.avi', '.mp3', '.wav', '.mkv', '.mp4', '.m4a', '.flac', '.ogg', '.opus', '.avif', '.hc', '.tc', '.xyz', '.exe', '.msi', '.tar', '.7z', '.tif', '.css', '.csv']
+        return any(unwanted_string in url.lower() for unwanted_string in unwanted_strings)
+
     def _is_blacklisted(self, url):
         return any(blacklisted_url in url for blacklisted_url in self._config["blacklisted_urls"])
 
     def _should_accept_url(self, url):
-        return url and self._is_valid_url(url) and not self._is_blacklisted(url)
+        return (
+            url 
+            and self._is_valid_url(url) 
+            and not self._is_blacklisted(url) 
+            and not self._contains_unwanted_strings(url)
+        )
 
     def _extract_urls(self, body, root_url):
         pattern = r"href=[\"'](?!#)(.*?)[\"'].*?"
@@ -158,51 +177,33 @@ class Crawler:
                     logging.debug("found {} links".format(len(self._links)))
                     self._browse_from_links()
             except (requests.exceptions.RequestException, UnicodeDecodeError):
-                logging.warning("\033[95mError connecting to root url: {}\033[0m".format(url))
-            except MemoryError:
-                logging.warning("\033[91mError: content at url: {} is exhausting the memory\033[0m".format(url))
-            except LocationParseError:
-                logging.warning("\033[93mError encountered during parsing of: {}\033[0m".format(url))
-            except self.CrawlerTimedOut:
-                logging.info("\033[91mTimeout has exceeded, exiting")
-                return
+                continue
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Crawler")
+    parser.add_argument("--config", "-c", default="config.json", help="Path to config file")
+    parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+    logging.getLogger("requests").setLevel(logging.CRITICAL)
+
+    try:
+        crawler = Crawler()
+        crawler.load_config_file(args.config)
+        crawler.crawl()
+    except KeyboardInterrupt:
+        logging.info("\033[91mCrawler interrupted by user\033[0m")
+    except Crawler.CrawlerTimedOut:
+        logging.info("\033[91mCrawler timed out\033[0m")
+
 
 def print_bandwidth_usage():
     global total_bandwidth
-    # Convert bytes to megabytes
-    mb_used = total_bandwidth / (1024 * 1024)
-    # Print the total bandwidth used, overwriting the previous line
-    print(f"\r\033[32mTotal Bandwidth Used:\033[0m \033[93m{mb_used:.2f}\033[0m \033[32mMB\033[0m", end="", flush=True)
+    print(f"\033[96mTotal bandwidth used: {total_bandwidth / (1024 * 1024):.2f} MB\033[0m", end="\r")
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--log", metavar="-l", type=str, help="logging level", default="info")
-    parser.add_argument("--config", metavar="-c", type=str, help="config file")
-    parser.add_argument("--timeout", metavar="-t", type=int, help="for how long the crawler should be running, in seconds", default=False)
-    parser.add_argument("--min_sleep", metavar="-min", type=int, help="Minimum sleep before clicking another link.")
-    parser.add_argument("--max_sleep", metavar="-max", type=int, help="Maximum sleep before clicking another link.")
-    args = parser.parse_args()
-
-    level = getattr(logging, args.log.upper())
-    logging.basicConfig(level=level)
-
-    crawler = Crawler()
-
-    config_file = args.config if args.config else os.path.join(os.path.dirname(__file__), 'config.json')
-    
-    try:
-        crawler.load_config_file(config_file)
-    except FileNotFoundError:
-        logging.error("Config file not found: {}".format(config_file))
-        return
-
-    if args.timeout:
-        crawler.set_option("timeout", args.timeout)
-    if args.min_sleep:
-        crawler.set_option("min_sleep", args.min_sleep)
-    if args.max_sleep:
-        crawler.set_option("max_sleep", args.max_sleep)
-    crawler.crawl()
 
 if __name__ == "__main__":
     main()
+
+

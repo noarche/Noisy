615922004220243e41726939b3ea441126e78d3c
noarche <n0arch@pm.me>
2024-08-07 23:55:18 -0400

    Update noisy.py

--- noisy.py
+++ noisy.py
@@ -1,11 +1,9 @@
 import argparse
 import datetime
 import json
-import logging
 import random
 import re
 import time
-import os
 from urllib.parse import urljoin, urlparse
 import requests
 from fake_useragent import UserAgent
@@ -36,7 +34,7 @@ infoabt = '''
       \033[94m    Traffic Obfuscation     \033[0m
       \033[95m****************************\033[0m
      \033[96m Build Date: August 6 2024 \033[0m
- \033[34m use no arguements for the default config\033[0m
+ \033[34m use no arguments for the default config\033[0m
  \033[31m Available arguments for other configs:\033[0m
  \033[32m config configHighBandwidth.json\033[0m
  \033[32m config configLowBandwidth.json\033[0m
@@ -68,11 +66,12 @@ class Crawler:
         try:
             response = requests.get(url, headers=headers, timeout=3)
             if response:
-                total_bandwidth += len(response.content)
-                print_bandwidth_usage()
+                content_length = len(response.content)
+                total_bandwidth += content_length
+                print_responsive_link(url)
+                print_bandwidth_usage(content_length)
             return response
         except requests.exceptions.RequestException as e:
-            logging.error(f"\033[91mRequest failed: {e}\033[0m")
             return None
 
     @staticmethod
@@ -130,13 +129,11 @@ class Crawler:
         max_depth = SYS_RANDOM.randint(1, self._config["max_depth"])
         is_depth_reached = depth >= max_depth
         if not self._links or is_depth_reached:
-            logging.debug("Hit a dead end, moving to the next root URL")
             return
         if self._is_timeout_reached():
             raise self.CrawlerTimedOut
         random_link = SYS_RANDOM.choice(self._links)
         try:
-            logging.info("\033[96mVisiting {}\033[0m".format(random_link))
             response = self._request(random_link)
             if response:
                 sub_page = response.content
@@ -146,9 +143,8 @@ class Crawler:
                     self._links = self._extract_urls(sub_page, random_link)
                 else:
                     self._remove_and_blacklist(random_link)
-                self.save_links(sub_links)  # Save the found links to a file
+                self.save_links(sub_links) 
         except (requests.exceptions.RequestException, UnicodeDecodeError):
-            logging.debug("Exception on URL: {}, removing from list and trying again!".format(random_link))
             self._remove_and_blacklist(random_link)
         self._browse_from_links(depth + 1)
 
@@ -178,12 +174,10 @@ class Crawler:
                 if response:
                     body = response.content
                     self._links = self._extract_urls(body, url)
-                    logging.debug("found {} links".format(len(self._links)))
                     self._browse_from_links()
                 else:
                     time.sleep(SYS_RANDOM.randrange(self._config["min_sleep"], self._config["max_sleep"]))
             except (requests.exceptions.RequestException, LocationParseError):
-                logging.error(f"\033[91mInvalid URL: {url}\033[0m")
                 continue
 
     def save_links(self, links):
@@ -191,8 +185,12 @@ class Crawler:
             for link in links:
                 file.write(link + '\n')
 
-def print_bandwidth_usage():
+def print_responsive_link(url):
+    print(f"\033[92mResponsive Link: {url}\033[0m")
+
+def print_bandwidth_usage(content_length=0):
     global total_bandwidth
+    total_bandwidth += content_length
     print(f"\033[34mTotal Bandwidth Used: {total_bandwidth / (1024 * 1024):.2f} MB\033[0m", end='\r')
 
 def main():
@@ -210,7 +208,6 @@ def main():
     crawler.crawl()
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
     main()
 
 

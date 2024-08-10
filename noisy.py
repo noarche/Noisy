9dbb8b5c8e72e4306f2ba6f8e033f360d4d27128
noarche <n0arch@pm.me>
2024-08-10 19:32:52 -0400

    Add files via upload

--- noisy.py
+++ noisy.py
@@ -1,6 +1,7 @@
 import argparse
 import datetime
 import json
+import os
 import random
 import re
 import time
@@ -8,42 +9,39 @@ from urllib.parse import urljoin, urlparse
 import requests
 from fake_useragent import UserAgent
 from urllib3.exceptions import LocationParseError
+import colorama
+from colorama import Fore, Style
 
-UA = UserAgent(min_percentage=15.1)
-REQUEST_COUNTER = -1
-SYS_RANDOM = random.SystemRandom()
-
-total_bandwidth = 0
-
+colorama.init(autoreset=True)
 
 
 main_logo = ''' 
-
-\033[92m  ░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓███████▓▒░▒▓█▓▒░░▒▓█▓▒░ \033[0m
-\033[92m  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ \033[0m
-\033[92m  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ \033[0m
-\033[92m  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░  \033[0m
-\033[92m  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░     \033[0m
-\033[92m  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░     \033[0m
-\033[92m  ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░▒▓███████▓▒░   ░▒▓█▓▒░     \033[0m                                                   
-
+      [91m_[0m[93m_[0m[92m_[0m           [96m_[0m[94m_[0m[95m_[0m                       [91m_[0m[93m_[0m[92m_[0m           [96m_[0m[94m_[0m[95m_[0m     
+     [91m/[0m[93m\[0m[92m_[0m[96m_[0m[94m\[0m         [95m/[0m[91m\[0m  [93m\[0m          [92m_[0m[96m_[0m[94m_[0m        [95m/[0m[91m\[0m  [93m\[0m         [92m|[0m[96m\[0m[94m_[0m[95m_[0m[91m\[0m    
+    [93m/[0m[92m:[0m[96m:[0m[94m|[0m  [95m|[0m       [91m/[0m[93m:[0m[92m:[0m[96m\[0m  [94m\[0m        [95m/[0m[91m\[0m  [93m\[0m      [92m/[0m[96m:[0m[94m:[0m[95m\[0m  [91m\[0m        [93m|[0m[92m:[0m[96m|[0m  [94m|[0m   
+   [95m/[0m[91m:[0m[93m|[0m[92m:[0m[96m|[0m  [94m|[0m      [95m/[0m[91m:[0m[93m/[0m[92m\[0m[96m:[0m[94m\[0m  [95m\[0m       [91m\[0m[93m:[0m[92m\[0m  [96m\[0m    [94m/[0m[95m:[0m[91m/[0m[93m\[0m [92m\[0m  [96m\[0m       [94m|[0m[95m:[0m[91m|[0m  [93m|[0m   
+  [92m/[0m[96m:[0m[94m/[0m[95m|[0m[91m:[0m[93m|[0m  [92m|[0m[96m_[0m[94m_[0m   [95m/[0m[91m:[0m[93m/[0m  [92m\[0m[96m:[0m[94m\[0m  [95m\[0m      [91m/[0m[93m:[0m[92m:[0m[96m\[0m[94m_[0m[95m_[0m[91m\[0m  [93m_[0m[92m\[0m[96m:[0m[94m\[0m[95m~[0m[91m\[0m [93m\[0m  [92m\[0m      [96m|[0m[94m:[0m[95m|[0m[91m_[0m[93m_[0m[92m|[0m[96m_[0m[94m_[0m 
+ [95m/[0m[91m:[0m[93m/[0m [92m|[0m[96m:[0m[94m|[0m [95m/[0m[91m\[0m[93m_[0m[92m_[0m[96m\[0m [94m/[0m[95m:[0m[91m/[0m[93m_[0m[92m_[0m[96m/[0m [94m\[0m[95m:[0m[91m\[0m[93m_[0m[92m_[0m[96m\[0m  [94m_[0m[95m_[0m[91m/[0m[93m:[0m[92m/[0m[96m\[0m[94m/[0m[95m_[0m[91m_[0m[93m/[0m [92m/[0m[96m\[0m [94m\[0m[95m:[0m[91m\[0m [93m\[0m [92m\[0m[96m_[0m[94m_[0m[95m\[0m     [91m/[0m[93m:[0m[92m:[0m[96m:[0m[94m:[0m[95m\[0m[91m_[0m[93m_[0m[92m\[0m
+ [96m\[0m[94m/[0m[95m_[0m[91m_[0m[93m|[0m[92m:[0m[96m|[0m[94m/[0m[95m:[0m[91m/[0m  [93m/[0m [92m\[0m[96m:[0m[94m\[0m  [95m\[0m [91m/[0m[93m:[0m[92m/[0m  [96m/[0m [94m/[0m[95m\[0m[91m/[0m[93m:[0m[92m/[0m  [96m/[0m    [94m\[0m[95m:[0m[91m\[0m [93m\[0m[92m:[0m[96m\[0m [94m\[0m[95m/[0m[91m_[0m[93m_[0m[92m/[0m    [96m/[0m[94m:[0m[95m/[0m[91m~[0m[93m~[0m[92m/[0m[96m~[0m   
+     [94m|[0m[95m:[0m[91m/[0m[93m:[0m[92m/[0m  [96m/[0m   [94m\[0m[95m:[0m[91m\[0m  [93m/[0m[92m:[0m[96m/[0m  [94m/[0m  [95m\[0m[91m:[0m[93m:[0m[92m/[0m[96m_[0m[94m_[0m[95m/[0m      [91m\[0m[93m:[0m[92m\[0m [96m\[0m[94m:[0m[95m\[0m[91m_[0m[93m_[0m[92m\[0m     [96m/[0m[94m:[0m[95m/[0m  [91m/[0m     
+     [93m|[0m[92m:[0m[96m:[0m[94m/[0m  [95m/[0m     [91m\[0m[93m:[0m[92m\[0m[96m/[0m[94m:[0m[95m/[0m  [91m/[0m    [93m\[0m[92m:[0m[96m\[0m[94m_[0m[95m_[0m[91m\[0m       [93m\[0m[92m:[0m[96m\[0m[94m/[0m[95m:[0m[91m/[0m  [93m/[0m     [92m\[0m[96m/[0m[94m_[0m[95m_[0m[91m/[0m      
+     [93m/[0m[92m:[0m[96m/[0m  [94m/[0m       [95m\[0m[91m:[0m[93m:[0m[92m/[0m  [96m/[0m      [94m\[0m[95m/[0m[91m_[0m[93m_[0m[92m/[0m        [96m\[0m[94m:[0m[95m:[0m[91m/[0m  [93m/[0m                 
+     [92m\[0m[96m/[0m[94m_[0m[95m_[0m[91m/[0m         [93m\[0m[92m/[0m[96m_[0m[94m_[0m[95m/[0m                     [91m\[0m[93m/[0m[92m_[0m[96m_[0m[94m/[0m                  
 '''
-
 infoabt = ''' 
-
-      \033[95m****************************\033[0m
-      \033[96m github.com/noarche/Noisy   \033[0m
-      \033[94m    Traffic Obfuscation     \033[0m
-      \033[95m****************************\033[0m
-     \033[96m Build Date: August 8 2024 \033[0m
- \033[34m Skip arguments for the default config\033[0m
- \033[36m To filter links enter string to filter.\033[0m
- \033[32m Enter minimum time to wait.\033[0m
- \033[32m Enter maximum time to wait.\033[0m
- \033[92m A random number between these two numbers will be used.\033[0m
-
- \033[34m Visit the github for more information.\033[0m
-
+\033[95m  ****************************\033[0m
+\033[96m   github.com/noarche/Noisy   \033[0m
+\033[94m      [91mT[0m[93mr[0m[92ma[0m[96mf[0m[94mf[0m[95mi[0m[91mc[0m [93mO[0m[92mb[0m[96mf[0m[94mu[0m[95ms[0m[91mc[0m[93ma[0m[92mt[0m[96mi[0m[94mo[0m[95mn[0m    \033[0m
+\033[95m  ****************************\033[0m
+\033[96m   Build Date: [91mA[0m[93mu[0m[92mg[0m[96mu[0m[94ms[0m[95mt[0m [91m1[0m[93m0[0m [92m2[0m[96m0[0m[94m2[0m[95m4[0m
+ [91mS[0m[93me[0m[92ml[0m[96me[0m[94mc[0m[95mt[0m [91mt[0m[93mh[0m[92me[0m [96mc[0m[94mo[0m[95mn[0m[91mf[0m[93mi[0m[92mg[0m [96my[0m[94mo[0m[95mu[0m [91mw[0m[93ma[0m[92mn[0m[96mt[0m [94mt[0m[95mo[0m [91mr[0m[93mu[0m[92mn[0m[96m,[0m [94mE[0m[95ma[0m[91ms[0m[93my[0m [92mt[0m[96mo[0m [94mb[0m[95ml[0m[91me[0m[93mn[0m[92md[0m [96mi[0m[94ms[0m [95mb[0m[91me[0m[93ms[0m[92mt[0m[96m.[0m [94mA[0m[95ml[0m[91ml[0m [93mo[0m[92mt[0m[96mh[0m[94me[0m[95mr[0m [91mq[0m[93mu[0m[92me[0m[96ms[0m[94mt[0m[95mi[0m[91mo[0m[93mn[0m[92ms[0m
+[96ma[0m[94mr[0m[95me[0m [91mo[0m[93mp[0m[92mt[0m[96mi[0m[94mo[0m[95mn[0m[91ma[0m[93ml[0m [92mt[0m[96mo[0m [94mc[0m[95mu[0m[91ms[0m[93mt[0m[92mo[0m[96mm[0m[94mi[0m[95mz[0m[91me[0m [93my[0m[92mo[0m[96mu[0m[94mr[0m [95me[0m[91mx[0m[93mp[0m[92me[0m[96mr[0m[94mi[0m[95me[0m[91mn[0m[93mc[0m[92me[0m [96m&[0m [94mO[0m[95mK[0m [91mt[0m[93mo[0m [92ms[0m[96mk[0m[94mi[0m[95mp[0m [91mi[0m[93mf[0m [92mu[0m[96mn[0m[94ms[0m[95mu[0m[91mr[0m[93me[0m[92m.[0m
+ \033[36m   To filter links enter string to filter. Not reccomended. Leave blank to continue.\033[0m
+  \033[32m   Domain Only? Not recomended. Leave blank to continue.\033[0m
+ \033[32m   Enter minimum time to wait. fast is 0 (reccomended: 0)\033[0m
+ \033[32m   Enter maximum time to wait. fast is 1 (reccomended: 4)\033[0m
+ \033[92m   A random number between these two numbers will be used.\033[0m
+ \033[96m   Enter depth, the number of links to visit on that domain before switching. A random number between 0 and your input will be used each time. Leave blank to continue.\033[0m
 '''
 
 
@@ -52,21 +50,35 @@ print(main_logo)
 print(infoabt)
 
 
+UA = UserAgent(min_percentage=15.1)
+REQUEST_COUNTER = -1
+SYS_RANDOM = random.SystemRandom()
+
+total_bandwidth = 0
+visited_responsive_urls = 0
+
 class Crawler:
     def __init__(self):
         self._config = {}
         self._links = []
         self._start_time = None
-        self._visit_counter = 0  
-        self._valid_string = None  
-        self._blacklist = set()  
+        self._visit_counter = 0
+        self._valid_string = None
+        self._blacklist = set()
+        self._domain_only = False
 
     def set_valid_string(self, valid_string):
         self._valid_string = valid_string
 
-    def _filter_links(self):
+    def set_domain_only(self, domain_only):
+        self._domain_only = domain_only
 
+    def _filter_links(self):
         if self._valid_string:
+            self._links = [link for link in self._links if self._valid_string in link]
+
+        if self._domain_only:
+            self._links = list(set(self._links))  # Remove duplicates
             self._links = [link for link in self._links if self._valid_string in link]
 
     def _normalize_link(self, link, root_url):
@@ -82,7 +94,7 @@ class Crawler:
         return link
 
     def _request(self, url):
-        global total_bandwidth
+        global total_bandwidth, visited_responsive_urls
         random_user_agent = UA.random
         headers = {"user-agent": random_user_agent}
         try:
@@ -90,6 +102,7 @@ class Crawler:
             if response:
                 content_length = len(response.content)
                 total_bandwidth += content_length
+                visited_responsive_urls += 1
                 print_responsive_link(url)
                 print_bandwidth_usage(content_length)
             return response
@@ -97,7 +110,6 @@ class Crawler:
             return None
 
     def _is_valid_url(self, url):
-
         try:
             result = urlparse(url)
             return all([result.scheme, result.netloc])
@@ -196,40 +208,63 @@ class Crawler:
                 file.write(link + '\n')
 
 def print_responsive_link(url):
-    print(f"\033[92mResponsive Link: {url}\033[0m")
+    print(f"{Fore.GREEN}[91mR[0m[93me[0m[92ms[0m[96mp[0m[94mo[0m[95mn[0m[91ms[0m[93mi[0m[92mv[0m[96me[0m [94mL[0m[95mi[0m[91mn[0m[93mk[0m: {url}{Style.RESET_ALL}")
 
 def print_bandwidth_usage(content_length=0):
-    global total_bandwidth
+    global total_bandwidth, visited_responsive_urls
     total_bandwidth += content_length
-    print(f"\033[95mTotal Bandwidth Used: {total_bandwidth / (1024 * 1024):.2f} MB\033[0m", end='\r')
+    print(f"{Fore.MAGENTA}[91mT[0m[93mo[0m[92mt[0m[96ma[0m[94ml[0m [95mB[0m[91ma[0m[93mn[0m[92md[0m[96mw[0m[94mi[0m[95md[0m[91mt[0m[93mh[0m [92mU[0m[96ms[0m[94me[0m[95md[0m: {total_bandwidth / (1024 * 1024):.2f} MB | [91mR[0m[93me[0m[92ms[0m[96mp[0m[94mo[0m[95mn[0m[91ms[0m[93mi[0m[92mv[0m[96me[0m [94mU[0m[95mR[0m[91mL[0m[93ms[0m [92mV[0m[96mi[0m[94ms[0m[95mi[0m[91mt[0m[93me[0m[92md[0m: {visited_responsive_urls}{Style.RESET_ALL}", end='\r')
+
+def load_configs():
+    config_dir = 'configs'
+    configs = [f for f in os.listdir(config_dir) if f.endswith('.json')]
+    print(f"{Fore.YELLOW}[91mA[0m[93mv[0m[92ma[0m[96mi[0m[94ml[0m[95ma[0m[91mb[0m[93ml[0m[92me[0m [96mC[0m[94mo[0m[95mn[0m[91mf[0m[93mi[0m[92mg[0m[96mu[0m[94mr[0m[95ma[0m[91mt[0m[93mi[0m[92mo[0m[96mn[0m[94ms[0m:{Style.RESET_ALL}")
+    for i, config in enumerate(configs):
+        print(f"{Fore.CYAN}{i + 1}. {config}{Style.RESET_ALL}")
+    return configs
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("config", nargs="?", help="Path to a configuration file")
-    args = parser.parse_args()
+    configs = load_configs()
+    if not configs:
+        print(f"{Fore.RED}No configuration files found in 'configs' directory.{Style.RESET_ALL}")
+        return
 
+    config_choice = input(f"{Fore.BLUE}Choose a configuration by number:{Style.RESET_ALL} ").strip()
+    try:
+        config_choice = int(config_choice) - 1
+        if 0 <= config_choice < len(configs):
+            selected_config = configs[config_choice]
+        else:
+            print(f"{Fore.RED}Invalid selection.{Style.RESET_ALL}")
+            return
+    except ValueError:
+        print(f"{Fore.RED}Invalid input.{Style.RESET_ALL}")
+        return
+
+    config_path = os.path.join('configs', selected_config)
     crawler = Crawler()
-    if args.config:
-        crawler.load_config_file(args.config)
-    else:
-        with open("config.json", "r") as default_config_file:
-            default_config = json.load(default_config_file)
-            crawler.set_config(default_config)
+    crawler.load_config_file(config_path)
 
-    valid_string = input("\033[32mEnter a valid string (e.g., .gov, .edu, .net) or leave blank to skip:\033[0m ").strip()
+    valid_string = input(f"{Fore.RED}Enter a valid string (e.g., .gov, .edu, .net) or leave blank to skip:{Style.RESET_ALL} ").strip()
     if valid_string:
         crawler.set_valid_string(valid_string)
 
-    min_sleep = input("\033[32mEnter min sleep time in seconds or leave blank to continue:\033[0m ").strip()
-    max_sleep = input("\033[32mEnter max sleep time in seconds or leave blank to continue:\033[0m ").strip()
+    domain_only = input(f"{Fore.RED}Domain only mode? (0 for no, 1 for yes, default is no):{Style.RESET_ALL} ").strip()
+    if domain_only == "1":
+        crawler.set_domain_only(True)
+
+    min_sleep = input(f"{Fore.GREEN}Enter min sleep time in seconds or leave blank to continue:{Style.RESET_ALL} ").strip()
+    max_sleep = input(f"{Fore.GREEN}Enter max sleep time in seconds or leave blank to continue:{Style.RESET_ALL} ").strip()
 
     if min_sleep.isdigit() and max_sleep.isdigit():
         crawler.set_option("min_sleep", int(min_sleep))
         crawler.set_option("max_sleep", int(max_sleep))
 
+    depth = input(f"{Fore.GREEN}Enter the maximum depth of hits:{Style.RESET_ALL} ").strip()
+    if depth.isdigit():
+        crawler.set_option("max_depth", int(depth))
+
     crawler.crawl()
 
 if __name__ == "__main__":
     main()
-
-
